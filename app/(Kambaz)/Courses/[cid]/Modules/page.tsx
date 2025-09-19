3c5a6b5d7d22b459aef92d2938ed7f78ff747338
jslaybaugh <slaybaugh.j@northeastern.edu>
2025-09-19 13:02:27 -0400

    Assignment 1 Pages

---
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -0,0 +1,47 @@
+export default function Modules() {
+  return (
+    <div>
+      <button>Collapse All</button>
+      <button>View Progress</button>
+      <button>Publish All</button>
+      <button>+ Module</button>
+      <ul id="wd-modules">
+        <li className="wd-module">
+          <div className="wd-title">Week 1</div>
+          <ul className="wd-lessons">
+            <li className="wd-lesson">
+              <span className="wd-title">LEARNING OBJECTIVES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">Introduction to the course</li>
+                <li className="wd-content-item">Learn what is Web Development</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li className="wd-module">
+          <div className="wd-title">Week 2</div>
+          <ul className="wd-lessons">
+            <li className="wd-lesson">
+              <span className="wd-title">LEARNING OBJECTIVES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">Create React Web Application</li>
+                <li className="wd-content-item">Get started with Assignment 1</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li className="wd-module">
+          <div className="wd-title">Week 3</div>
+          <ul className="wd-lessons">
+            <li className="wd-lesson">
+              <span className="wd-title">LEARNING OBJECTIVES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">Create User Interfaces with HTML</li>
+                <li className="wd-content-item">Deploy Assignment 1</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+);}
