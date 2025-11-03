a8c4603f934a3788bca4cd744529cc8f868d7ad3
jslaybaugh <slaybaugh.j@northeastern.edu>
2025-11-03 18:20:07 -0500

    last fix

--- app/(Kambaz)/Dashboard/page.tsx
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -9,7 +9,7 @@ import { addNewCourse, deleteCourse, updateCourse } from "../Courses/reducer";
 import { RootState } from "../store";
 export default function Dashboard() {
   const { courses } = useSelector((state: RootState) => state.coursesReducer);
-  const currentUser: any = useSelector((state: RootState) => state.accountReducer);
+  const { currentUser }: any = useSelector((state: RootState) => state.accountReducer);
   const { enrollments } = db;
   const dispatch = useDispatch();
   const [course, setCourse] = useState<any>({
