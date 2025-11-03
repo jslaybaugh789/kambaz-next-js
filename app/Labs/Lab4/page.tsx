90cd98a8fd259fc6ebd3b2c2901c7b828a6f28f5
jslaybaugh <slaybaugh.j@northeastern.edu>
2025-11-03 17:51:19 -0500

    page/index swap for redux examples

--- app/Labs/Lab4/page.tsx
+++ app/Labs/Lab4/page.tsx
@@ -10,7 +10,7 @@ import DateStateVariable from "./DateStateVariable";
 import ObjectStateVariable from "./ObjectStateVariable";
 import ArrayStateVariable from "./ArrayStateVariable";
 import ParentStateComponent from "./ParentStateComponent";
-import ReduxExamples from "./ReduxExamples/page";
+import ReduxExamples from "./ReduxExamples";
 import store from "./store";
 import { Provider } from "react-redux";
 
@@ -34,7 +34,6 @@ export default function Lab3() {
             <ArrayStateVariable/>
             <ParentStateComponent/>
             <ReduxExamples/>
-
         </div>
         </Provider>
     );
